776933fbca2f2b5bdf2c6429ef8b109f32fa4c43
louhergetup <834456892@qq.com>
2022-11-07 21:45:54 +0800

    第六次提交 修改路径

--- LSTM_Single/lstm_model.py
+++ LSTM_Single/lstm_model.py
@@ -115,7 +115,7 @@ def forecast_lstm(model, batch_size, X):
     
 
 # 加载数据
-series = read_csv('data.csv',encoding='gbk', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series = read_csv('LSTM_Single/data.csv',encoding='gbk', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
 # 最后N条数据作为测试数据
 testNum = 864
 
